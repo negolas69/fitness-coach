092da8e3c335319b84ea5663d7608a08c67c91e5
DerFliegendeHollaender <bleronregja1@gmail.com>
2023-11-28 09:15:16 +0100

    Struktur + Design

---
+++ Code/frontend/scripts/main.js
@@ -0,0 +1,6 @@
+function signUp() {
+  const username = document.getElementById("signUp");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const password2 = document.getElementById("password2");
+}
