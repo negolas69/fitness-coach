aedd3fe599a293f3d14839e0787910e10db5b89b
negolas69 <128027907+negolas69@users.noreply.github.com>
2023-12-12 10:05:20 +0100

    ChatGPT Implementation

---
+++ Code/frontend/scripts/workout.js
@@ -0,0 +1,19 @@
+document.getElementById("chat-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const prompt = document.getElementById("prompt").value;
+  fetch("/api/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ question: prompt }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("response").innerText = data.response;
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      document.getElementById("response").innerText = "An error occurred.";
+    });
+});
--- Code/index.js
+++ Code/index.js
@@ -1,14 +1,42 @@
 import express from "express";
 import { router as userRouter } from "./backend/user/user.router.js";
 import cors from "cors";
+import dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
 const app = express();
 
 app.use(express.static("frontend"));
 app.use(cors());
-
 app.use(express.json());
 app.use("/api/user", userRouter);
 
-app.listen(3001, () => {
-  console.log("Server listens to http://localhost:3001");
+const apiKey = process.env.OPEN_AI_KEY;
+console.log("API Key:", apiKey);
+// OpenAI Configuration
+const openai = new OpenAI({ apiKey: apiKey });
+
+app.post("/api/query", async (req, res) => {
+  const question = req.body.question;
+
+  try {
+    const response = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      prompt: question,
+      max_tokens: 150,
+    });
+
+    console.log(response);
+
+    res.json({ response: response.choices[0].text });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
 });
